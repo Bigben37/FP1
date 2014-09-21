b15623c1a6a7dd55155e7f21ed5f645a9ae13b3c
Bigben37 <benjamin@dierottlers.de>
2014-09-21 13:14:24 +0200

    seperated y axis title and tick labels

--- 0916-I2/src/fit_lambda.py
+++ 0916-I2/src/fit_lambda.py
@@ -3,26 +3,27 @@ from ROOT import gROOT, gStyle, TCanvas, TLegend
 from data import Data
 from fitter import Fitter
 
+
 def fitLambda():
     data = Data()
     data.addPoint(500, 1.000279)
     data.addPoint(540, 1.000278)
     data.addPoint(600, 1.000277)
     data.addPoint(682, 1.000276)
-    
+
     c = TCanvas('c1', '', 1280, 720)
     g = data.makeGraph('g', 'Wellenl#ddot{a}nge #lambda', 'Brechungsindex n')
-    #TODO custom labels
-    #print(g.GetYaxis().FindBin(1.0002765))
-    #g.GetYaxis().SetBinLabel(g.GetYaxis().FindBin(1.000276), '2.76')
+    # TODO custom labels
+    g.GetYaxis().SetLabelSize(0.03)
+    g.GetYaxis().SetTitleOffset(1.39)
     g.Draw('AP')
-    
+
     fit = Fitter('f', '[0]+[1]*x')
     fit.setParam(0, 'a', 2)
     fit.setParam(1, 'b', -1)
     fit.fit(g, 450, 700)
     fit.saveData('../calc/fit_lambda.txt', 'w')
-    
+
     a = fit.params[0]['value']
     sa = fit.params[0]['error']
     b = fit.params[1]['value']
@@ -32,15 +33,15 @@ def fitLambda():
     l.AddEntry('g', 'Brechungsindex in Abh#ddot{a}ngigkeit der Wellenl#ddot{a}nge', 'p')
     l.AddEntry(fit.function, 'Fit mit n(#lambda)= a+b*#lambda', 'l')
     l.AddEntry(0, '#chi^{2} / DoF : %f' % fit.getChisquareOverDoF(), '')
-    l.AddEntry(0, 'Paramter:', '') 
+    l.AddEntry(0, 'Paramter:', '')
     l.AddEntry(0, 'a: %e #pm %e' % (a, sa), '')
-    l.AddEntry(0, 'b: %e #pm %e' % (b, sb), '')   
+    l.AddEntry(0, 'b: %e #pm %e' % (b, sb), '')
     l.SetTextSize(0.03)
     l.Draw()
-    
+
     c.Update()
     c.Print('../img/fit_lambda.pdf', 'pdf')
-    
+
 
 def main():
     gROOT.Reset()

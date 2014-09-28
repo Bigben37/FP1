ed167844a3037e7023388e5e90d613850beeba4b
Bigben37 <benjamin@dierottlers.de>
2014-09-28 22:59:49 +0200

    added underground peak energy

--- 0919-Szint.zähler/src/eval_underground.py
+++ 0919-Szint.zähler/src/eval_underground.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python2.7
 from ROOT import gROOT, gStyle, TCanvas, TLegend
-from szint import SzintData
+from szint import SzintData, channelToEnergy
 from fitter import Fitter
+import functions as funcs
+from txtfile import TxtFile
 
 
 def evalUnderground():
+    # load data
     data = SzintData.fromPath('../data/untergrund.TKA')
     data.convertToCountrate()
+    gauge = funcs.loadCSVToList('../calc/energy_gauge_raw.txt')
 
+    # make canvas and graph
     c = TCanvas('c', '', 1280, 720)
     c.SetLogy()
     g = data.makeGraph()
     g.SetMarkerStyle(1)
     g.Draw('APX')
 
+    # fit
     fit = Fitter('f', 'pol1(0) + gaus(2)')
     fit.setParam(0, 'a', 2e-3)
     fit.setParam(1, 'b', 0)
@@ -22,8 +28,13 @@ def evalUnderground():
     fit.setParam(4, 's1', 200)
     fit.fit(g, 3250, 4050)
     fit.saveData('../calc/underground_peak.txt', 'w')
-    
-    #TODO Legend
+
+    p, pc = fit.params[3]['value'], fit.params[4]['error']
+    E, sE = channelToEnergy(p, pc, gauge)
+    with TxtFile('../calc/underground_peak_energy.txt', 'w') as f:
+        f.writeline('\t', *map(lambda x: str(x), (p, pc, E, sE)))
+
+    # TODO Legend
 
     c.Update()
     c.Print('../img/underground.pdf')

0496e39343a1cdd9f158df8586e04aed2fe21714
Bigben37 <benjamin@dierottlers.de>
2014-09-28 18:15:42 +0200

    converted underground to countrate

--- 0919-Szint.zähler/src/eval_underground.py
+++ 0919-Szint.zähler/src/eval_underground.py
@@ -6,21 +6,24 @@ from fitter import Fitter
 
 def evalUnderground():
     data = SzintData.fromPath('../data/untergrund.TKA')
+    data.convertToCountrate()
 
     c = TCanvas('c', '', 1280, 720)
     c.SetLogy()
     g = data.makeGraph()
     g.SetMarkerStyle(1)
-    g.Draw('AP')
+    g.Draw('APX')
 
     fit = Fitter('f', 'pol1(0) + gaus(2)')
-    fit.setParam(0, 'a', 100)
-    fit.setParam(1, 'b', -0.1)
-    fit.setParam(2, 'A1', 1000)
+    fit.setParam(0, 'a', 2e-3)
+    fit.setParam(1, 'b', 0)
+    fit.setParam(2, 'A1', 1e-2)
     fit.setParam(3, 'c1', 3500)
     fit.setParam(4, 's1', 200)
     fit.fit(g, 3250, 4050)
     fit.saveData('../calc/underground_peak.txt', 'w')
+    
+    #TODO Legend
 
     c.Update()
     c.Print('../img/underground.pdf')

0f95dade937ceccb1f78ed925d31b2ae9fb1ea6b
Bigben37 <benjamin@dierottlers.de>
2014-09-29 15:21:12 +0200

    changed energy gauge to polynom 2nd order

--- 0919-Szint.zähler/src/szint.py
+++ 0919-Szint.zähler/src/szint.py
@@ -38,22 +38,25 @@ class SzintData(DataErrors):
         self.subtractUnderground()
 
 
-def channelToEnergy(c, sc, gauge):
+def channelToEnergy(x, sx, gauge):
     """convert channel to energy with calculated energy gauge
 
     Arguments:
-    c     -- channel
-    sc    -- error of channel
+    x     -- channel
+    sx    -- error of channel
     gauge -- fit parameter of energy-channel fit
     """
-    # set params and erros of E = a + b*c
+    # set params and erros of E = a + b*x + x*x^2
     a, sa = gauge[0]
     b, sb = gauge[1]
-    rho = gauge[2][0]
+    c, sc = gauge[2]
+    rhoab = gauge[3][0]
+    rhoac = gauge[4][0]
+    rhobc = gauge[5][0]
     # calculate energy and error
-    E = a + b * c
-    sE = np.sqrt(sa ** 2 + c * rho * sa * sb + (c * sb) ** 2 + (b * sc) ** 2)
-
+    E = a + b * x + c * (x ** 2)
+    sE = np.sqrt(sa ** 2 + 2 * rhoab * sa * sb * x + (sb * x) ** 2 + 2 * rhoac * sa * sc * (x ** 2)
+                 + 2 * rhobc * sb * sc * (x ** 3) + (sc ** 2) * (x ** 4) + (sx * (b + 2 * c * x)) * 2)
     return E, sE
 
 

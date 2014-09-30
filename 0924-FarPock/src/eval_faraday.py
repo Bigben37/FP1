8d3c3aed73d910b3329342b4caf99d8495f699fb
Bigben37 <benjamin@dierottlers.de>
2014-09-30 22:04:51 +0200

    evaluation for faraday effect

---
+++ 0924-FarPock/src/eval_faraday.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python2.7
+from ROOT import gROOT, gStyle, TCanvas, TLegend, TLine
+from farpock import FaraData
+from fitter import Fitter
+from txtfile import TxtFile
+
+
+def evalFaraday():
+    data = FaraData.fromPath('../data/fara_data.txt')
+
+    c = TCanvas('c', '', 1280, 720)
+    g = data.makeGraph('g', 'Strom I / A', 'Winkel #alpha / #circ')
+    g.GetXaxis().SetRangeUser(-6, 6)
+    g.SetMinimum(-15)
+    g.SetMaximum(15)
+    g.Draw('AP')
+
+    vline = TLine(0, -15, 0, 15)
+    vline.Draw()
+    hline = TLine(-6, 0, 6, 0)
+    hline.Draw()
+
+    fit = Fitter('f', 'pol1(0)')
+    fit.setParam(0, 'a', 0)
+    fit.setParam(1, 'b', 2)
+    fit.fit(g, -5.5, 5.5)
+    fit.saveData('../calc/faraday.txt', 'w')
+
+    l = TLegend(0.15, 0.65, 0.35, 0.85)
+    l.AddEntry('g', 'Messwerte', 'p')
+    l.AddEntry(fit.function, 'Fit mit y = a + b*x', 'l')
+    l.AddEntry(0, 'Parameter:', '')
+    l.AddEntry(0, 'a = %.2f #pm %.2f' % (fit.params[0]['value'], fit.params[0]['error']), '')
+    l.AddEntry(0, 'b = %.3f #pm %.3f' % (fit.params[1]['value'], fit.params[1]['error']), '')
+    l.Draw()
+
+    c.Update()
+    c.Print('../img/faraday.pdf', 'pdf')
+
+    m = (fit.params[1]['value'], fit.params[1]['error'])
+    c = 2556
+    v = map(lambda x: x / c, m)  # verdet constant
+    oe = 60 * 79.58 / 100  # factor for min/(Oe*cm)
+    voe = map(lambda x: x * oe, v)  # verdet in min/(Oe*cm)
+    with TxtFile('../calc/faraday_verdet.txt', 'w') as f:
+        f.writeline('\t', *map(str, v))
+        f.writeline('\t', *map(str, voe))
+
+
+def main():
+    gROOT.Reset()
+    gROOT.SetStyle('Plain')
+    gStyle.SetPadTickY(1)
+    gStyle.SetPadTickX(1)
+    evalFaraday()
+
+if __name__ == "__main__":
+    main()
--- 0924-FarPock/src/farpock.py
+++ 0924-FarPock/src/farpock.py
@@ -1,12 +1,14 @@
 from data import DataErrors
 import os
+import numpy as np
+
 
 class PockData(DataErrors):
-    
+
     def __init__(self):
         super(PockData, self).__init__()
         self.ycol = 1
-    
+
     @classmethod
     def fromPath(cls, path, ycol):
         data = cls()
@@ -18,10 +20,7 @@ class PockData(DataErrors):
             except NameError:
                 print(data.__class__.__name__ + ".loadData() not in scope! Please implement. ")
         return data
-    
-    #def loadData(self):
-        #print(self.ycol)
-        
+
     def loadData(self):
         d = os.path.dirname(os.path.abspath(__file__))
         path = os.path.abspath(os.path.join(d, self.path))
@@ -31,6 +30,22 @@ class PockData(DataErrors):
                 if firstline:
                     firstline = False
                 else:
-                    row = row.strip().split('\t')                
-                    self.addPoint(float(row[0]), float(row[self.ycol]), 0,0)
-                
+                    row = row.strip().split('\t')
+                    self.addPoint(float(row[0]), float(row[self.ycol]), 0, 0)
+
+
+class FaraData(DataErrors):
+
+    def loadData(self):
+        d = os.path.dirname(os.path.abspath(__file__))
+        path = os.path.abspath(os.path.join(d, self.path))
+        with open(path, 'r') as f:
+            for row in f:
+                data = row.strip().split('\t')
+                x = float(data[0])
+                xe = 0.1
+                y = -np.average(map(float, data[1:]))  # take average and invert
+                if y < -90:
+                    y = y + 180
+                ye = 0.2 / np.sqrt(len(data[1:]))
+                self.addPoint(x, y, xe, ye)

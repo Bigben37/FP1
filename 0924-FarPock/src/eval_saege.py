93cafbc5e65f3e3943f9c7a3f5615a84ab632d99
Standard24 <bitterling@quantentunnel.de>
2014-09-30 21:24:09 +0200

    wrote analysis stuff for sawtooth-pockels experiment

--- 0924-FarPock/src/eval_saege.py
+++ 0924-FarPock/src/eval_saege.py
@@ -1,6 +1,9 @@
 from farpock import PockData
 from fitter import Fitter
 from ROOT import gROOT, gStyle, TCanvas, TLegend
+from functions import avgerrors
+import numpy as np
+from txtfile import TxtFile
 
 #eval methoden
 
@@ -16,7 +19,23 @@ def main():
         param = eval('pock_saege_winkel%d' % i, fitlist[i])
         if fitlist[i]:
             params.append(param)
-    print(params)
+            
+
+    difflist = []            
+    for i in xrange(3):
+        difflist.append((params[i][1][0]-params[i][0][0],np.sqrt(params[i][1][1]**2 + params[i][0][1]**2)))
+    
+    print(difflist)
+    print(zip(*difflist))
+    av = avgerrors(*zip(*difflist))
+    print(av)
+    
+    with TxtFile('../calc/diff_minmax.txt', 'w') as f:
+        for dif in difflist:
+            f.writeline('\t', *map(str, dif))
+        f.writeline('\t', str(av[0]),str(av[1]),'weighted average')
+      
+   
 
 def eval(name,f):
     """description
@@ -49,13 +68,13 @@ def eval(name,f):
     paramlist = []
     
     if f:
-        fit1 = Fitter('f1', '[0]+[1]*x')
-        fit1.function.SetLineWidth(2)
-        fit1.function.SetLineColor(1)
-        fit1.setParam(0, 'a', -5)
-        fit1.setParam(1, 'b', 300)
-        fit1.fit(g1, 0.015, 0.029)
-        fit1.saveData('../calc/fit1.txt', 'w')
+        #fit1 = Fitter('f1', '[0]+[1]*x')
+        #fit1.function.SetLineWidth(2)
+        #fit1.function.SetLineColor(1)
+        #fit1.setParam(0, 'a', -5)
+        #fit1.setParam(1, 'b', 300)
+        #fit1.fit(g1, 0.015, 0.029)
+        #fit1.saveData('../calc/fit1.txt', 'w')
         
         fit2 = Fitter('f2', '[0]*(x-[1])**2+[2]')
         fit2.function.SetLineWidth(2)
@@ -64,7 +83,7 @@ def eval(name,f):
         fit2.setParam(1, 's', 0.017)
         fit2.setParam(2, 'c', -2)
         fit2.fit(g2, 0.015, 0.0202, '+')
-        fit2.saveData('../calc/fit2.txt', 'w')
+        #fit2.saveData('../calc/fit2.txt', 'w')
         
         fit3 = Fitter('f2', '[0]*(x-[1])**2+[2]')
         fit3.function.SetLineWidth(2)
@@ -74,13 +93,13 @@ def eval(name,f):
         fit3.setParam(1, 's', 0.023)
         fit3.setParam(2, 'c', 2)
         fit3.fit(g2, 0.0202, 0.0275, '+')
-        fit3.saveData('../calc/fit3.txt', 'w')
+        #fit3.saveData('../calc/fit3.txt', 'w')
     
         l = TLegend(0.635, 0.52, 0.87, 0.87)
         l.AddEntry('g1', 'S#ddot{a}gezahnspannung', 'p')
-        l.AddEntry(fit1.function, 'Fit mit y = a + b*x', 'l')
-        l.AddEntry(0, 'a = %.3f #pm %.3f' % (fit1.params[0]['value'], fit1.params[0]['error']), '')
-        l.AddEntry(0, 'b = %.1f #pm %.1f' % (fit1.params[1]['value'], fit1.params[1]['error']), '')
+        #l.AddEntry(fit1.function, 'Fit mit y = a + b*x', 'l')
+        #l.AddEntry(0, 'a = %.3f #pm %.3f' % (fit1.params[0]['value'], fit1.params[0]['error']), '')
+        #l.AddEntry(0, 'b = %.1f #pm %.1f' % (fit1.params[1]['value'], fit1.params[1]['error']), '')
         l.AddEntry('g2', 'Signal der Photodiode', 'p')
         l.AddEntry(fit2.function, 'Fit mit y = a1 * (x-s1)^2 + b1', 'l')
         l.AddEntry(0, 's1 = %.6f #pm %.6f' % (fit2.params[1]['value'], fit2.params[1]['error']), '')
@@ -89,9 +108,7 @@ def eval(name,f):
         l.SetFillColor(0)
         l.Draw()
         
-        paramlist = [(fit1.params[0]['value'],fit1.params[0]['error']),
-                     (fit1.params[1]['value'], fit1.params[1]['error']),
-                     (fit2.params[1]['value'], fit2.params[1]['error']),
+        paramlist = [(fit2.params[1]['value'], fit2.params[1]['error']),
                      (fit3.params[1]['value'], fit3.params[1]['error'])]
         
     else:

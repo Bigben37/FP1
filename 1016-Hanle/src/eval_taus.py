d676454c14a3456527204837f03b86e1ee74e440
Bigben37 <benjamin@dierottlers.de>
2014-10-24 19:34:35 +0200

    fit taus with T error

--- 1016-Hanle/src/eval_taus.py
+++ 1016-Hanle/src/eval_taus.py
@@ -9,23 +9,15 @@ from hanle import setMultiGraphTitle, TempToSeries, serieslabels, seriescolors, 
 import numpy as np
 
 
-def TempToPressure(T):
-    T2 = 273.15 + T
-    pc = 167000000.
-    Tc = 1764.
-    Tr = 1. - T2 / Tc
-    a1 = -4.57618368
-    a2 = -1.40726277
-    a3 = 2.36263541
-    a4 = -31.0889985
-    a5 = 58.0183959
-    a6 = -27.6304546
-    P = 1000 * pc * np.exp((Tc / T2) * (a1 * Tr + a2 * Tr ** 1.89 + a3 * Tr ** 2 + a4 * Tr ** 8 +
-                                           a5 * Tr ** 8.5 + a6 * Tr ** 9))  # in mPa
-    return P
+def loadTempDict():
+    data = loadCSVToList('../calc/temp_pressure.txt')
+    d = dict()
+    for T, p, sp in data:
+        d[T] = (p, sp)
+    return d
 
 
-def printOverview(datas, phi, mintau, maxtau):
+def printGraph(datas, phi, name='', fit=False):
     # make graphs
     c = TCanvas('c_%d' % phi)
     graphs = TMultiGraph()
@@ -37,18 +29,29 @@ def printOverview(datas, phi, mintau, maxtau):
         graphs.Add(g)
     graphs.Draw('AP')
     gPad.Update()
-    graphs.SetMinimum(mintau - 5)
-    graphs.SetMaximum(maxtau + 5)
-    graphs.GetXaxis().SetLimits(0, 230)
     setMultiGraphTitle(graphs, 'Druck p / mPa', '#tau / ns')
 
-    l = TLegend(0.6, 0.15, 0.85, 0.3)
+    if fit:
+        fit = Fitter('fit_%d' % phi, 'pol1(0)')
+        fit.function.SetNpx(1000)
+        fit.setParam(0, '#tau_{0}', 119)
+        fit.setParam(1, 'm', 0.5)
+        fit.fit(graphs, 0, 225, 'M')
+        fit.saveData('../calc/fit_tau_%02d%s.txt' % (phi, name), 'w')
+
+    if fit:
+        l = TLegend(0.6, 0.15, 0.85, 0.5)
+    else:
+        l = TLegend(0.6, 0.15, 0.85, 0.3)
     for i, graph in enumerate(graphs.GetListOfGraphs()):
         l.AddEntry(graph, serieslabels[i], 'p')
+    if fit:
+        l.AddEntry(fit.function, 'Fit mit #tau(p) = #tau_{0} + m * p', 'l')
+        fit.addParamsToLegend(l, [('%.1f', '%.1f'), ('%.2f', '%.2f')], chisquareformat='%.2f', advancedchi=True, units=['ns', 'ns / mPa'])
     l.Draw()
 
     c.Update()
-    c.Print('../img/taus_%02d.pdf' % phi, 'pdf')
+    c.Print('../img/taus_%02d%s.pdf' % (phi, name), 'pdf')
 
 
 def makeFit(datalist, phi, name=''):
@@ -62,7 +65,7 @@ def makeFit(datalist, phi, name=''):
     fit.setParam(0, '#tau_{0}', 119)
     fit.setParam(1, 'm', 0.5)
     fit.fit(g, 0, 225, 'M')
-    fit.saveData('../calc/fit_tau_%02d.txt' % phi, 'w')
+    fit.saveData('../calc/fit_tau_%02d%s.txt' % (phi, name), 'w')
 
     l = TLegend(0.55, 0.15, 0.85, 0.4)
     l.SetTextSize(0.03)
@@ -79,21 +82,23 @@ def makeFit(datalist, phi, name=''):
 
 def main():
     phis = [0, 45, 90]
+    tempDict = loadTempDict()
     for phi in phis:
         # read data
         datalist = loadCSVToList('../calc/taus_%02d.txt' % phi)
         datas = {0: [], 1: [], 2: []}
-        mintau = min(zip(*datalist)[1])
-        maxtau = max(zip(*datalist)[1])
-        for T, tau, stau in datalist:
-            p = TempToPressure(T)
-            sp = p * STEMP / (T + 273.15)
+        for T, tau, stau, fitphi, sfitphi in datalist:
+            p, sp = tempDict[T]
             datas[TempToSeries(T)].append((p, tau, sp, stau))  # sort in right series list
 
-        printOverview(datas, phi, mintau, maxtau)
-        makeFit(datas[0] + datas[1] + datas[2], phi, '_total')
-        makeFit(datas[1], phi, '_partial')
-        makeFit(datas[1] + datas[2], phi, '_day2')
+        printGraph(datas, phi)
+        printGraph(datas, phi, '_total', True)
+
+        del datas[0]
+        printGraph(datas, phi, '_day2', True)
+
+        del datas[2]
+        printGraph(datas, phi, '_partial', True)
 
 
 if __name__ == '__main__':
